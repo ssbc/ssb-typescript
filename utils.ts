5e7dfcec5868192e37ac01dd0e0a010dfd587add
Andre Alves Garzia <andre@andregarzia.com>
2021-05-13 14:44:22 +0000

    [[ enhancement ]] support for msgs of type blog.
    * Added `BlogContent` type.
    * Added `isBlogMsg()` function.

--- utils.ts
+++ utils.ts
@@ -1,16 +1,17 @@
 import {
   Msg,
   PostContent,
+  BlogContent,
   AboutContent,
   ContactContent,
   VoteContent,
   UnboxedMsg,
   MsgId,
   MsgInThread,
-} from './readme';
+} from "./readme";
 
 export function isMsg(msg: any): msg is Msg<any> {
-  return msg && msg.key && msg.value && typeof msg.value === 'object';
+  return msg && msg.key && msg.value && typeof msg.value === "object";
 }
 
 export function isRootMsg(msg: Msg<any>): boolean {
@@ -29,7 +30,7 @@ export function isIndirectReplyMsgToRoot(rootKey: MsgId) {
 }
 
 export function isPostMsg(msg: Msg<any>): msg is Msg<PostContent> {
-  return msg?.value?.content?.type === 'post';
+  return msg?.value?.content?.type === "post";
 }
 
 export function isRootPostMsg(msg: Msg<any>): msg is Msg<PostContent> {
@@ -41,27 +42,31 @@ export function isReplyPostMsg(msg: Msg<any>): msg is Msg<PostContent> {
 }
 
 export function isAboutMsg(msg: Msg<any>): msg is Msg<AboutContent> {
-  return msg?.value?.content?.type === 'about';
+  return msg?.value?.content?.type === "about";
 }
 
 export function isContactMsg(msg: Msg<any>): msg is Msg<ContactContent> {
-  return msg?.value?.content?.type === 'contact';
+  return msg?.value?.content?.type === "contact";
 }
 
 export function isVoteMsg(msg: Msg<any>): msg is Msg<VoteContent> {
-  return msg?.value?.content?.type === 'vote';
+  return msg?.value?.content?.type === "vote";
 }
 
 export function isPrivate(msg: Msg<any> | UnboxedMsg): boolean {
-  if ((msg as UnboxedMsg).meta?.private) return true
+  if ((msg as UnboxedMsg).meta?.private) return true;
   if ((msg as UnboxedMsg).value.private) return true;
   if (Array.isArray(msg.value.content?.recps)) return true;
-  return typeof msg?.value?.content === 'string';
+  return typeof msg?.value?.content === "string";
 }
 
 export function isPublic(msg: Msg<any> | UnboxedMsg): boolean {
-  if ((msg as UnboxedMsg).meta?.private) return false
+  if ((msg as UnboxedMsg).meta?.private) return false;
   if ((msg as UnboxedMsg).value.private) return false;
   if (Array.isArray(msg.value.content?.recps)) return false;
-  return typeof msg?.value?.content !== 'string';
+  return typeof msg?.value?.content !== "string";
+}
+
+export function isBlogMsg(msg: Msg<any>): msg is Msg<BlogContent> {
+  return msg?.value?.content?.type === "blog";
 }
